6f630a8f7da8c9f112096ed72d77ea83c7f2b97b
GerGoo-ph <grg.kollar@gmail.com>
2026-01-31 11:55:04 +0100

    Update p14HF.cpp

--- p14HF.cpp
+++ p14HF.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
-{
-    for(int i=1; i<=10;i++){
+{for(int i=1; i<=10;i++){
         for(int j=1; j<= 10;j++){
-            if(i*j < 100){
-                cout << " ";
-                if(i*j < 10){
-                    cout << " ";
-                }
-            }
-            cout << (j * i) << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+            if(i*j < 100){ cout << " ";
+                if(i*j < 10){ cout << " ";} 
+            }cout << (j * i) << " "; 
+        }cout << endl; 
+    }return 0;
 }
