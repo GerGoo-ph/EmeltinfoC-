b8d3f470af3ecfb38f4c74bce399f1b848409071
GerGoo-ph <grg.kollar@gmail.com>
2026-01-31 10:58:39 +0100

    Add files via upload

---
+++ p9 (1).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    string name;
+    cout << "Mi a neved?" << endl;
+    getline(cin,name);
+    cout << "Lépcsõzetesen" << endl;
+    for(int i=1;i<=7;i++){
+            for(int j=5;j>=i-1;j--){
+                cout << " ";
+            }
+        cout << name << endl;
+    }
+
+    return 0;
+}
